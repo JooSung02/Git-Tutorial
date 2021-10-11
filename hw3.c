58fe7505f18b7bcfc97b9dcf41f372fca5e60423
Joosung02 <wntjdqkr156@gmail.com>
2021-10-12 07:46:07 +0900

    Add New File [hw3.c]

---
+++ hw3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int num;
+    int i;
+
+
+    printf("Please enter a number : ");
+    scanf("%d", &num);
+
+
+    for (i = 2; i < num; i++) {
+        if (num % i == 0)
+            break;
+    }
+
+
+    if (i == num)
+        printf("It is a prime number.");
+    else
+        printf("It is not a prime number.");
+
+
+}
+
