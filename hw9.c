614933441b57d7fb44dd50a7c6396e4f228e2daf
JooSung02 <wntjdqkr156@gmail.com>
2021-12-06 18:42:28 +0900

    Add New File [hw9.c]

---
+++ hw9.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+typedef struct cities
+{
+	char country[20];
+	char city[20];
+	int population;
+} cities;
+
+void Showinf(cities* cptr)
+{
+	printf("%d. %s in %s with a population of %d people.");
+}
+
+int main(void)
+{
+	cities arr[3];
+	int i;
+	printf("Input three cities \n");
+	for (i = 0; i < 3; i++)
+	{
+		printf("Name >"); scanf("%s", arr[i].city);
+		printf("Country >"); scanf("%s", arr[i].country);
+		printf("Population >"); scanf("%d", arr[i].population);
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		Showinf(&arr[i]);
+	}
+	return 0;
+}
+
